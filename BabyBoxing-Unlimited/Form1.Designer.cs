5eafc5a53519889bf2ee41d0b4543b3033c16c9d
MadamBork <sorine.fairfield@gmail.com>
2026-02-11 17:38:52 -0500

    Baby Boxing - sounds, music, base tick function outline

--- BabyBoxing-Unlimited/Form1.Designer.cs
+++ BabyBoxing-Unlimited/Form1.Designer.cs
@@ -28,31 +28,46 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             this.debugLabel1 = new System.Windows.Forms.Label();
             this.playerBabyImageLabel = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.timerTitlescreenEnter = new System.Windows.Forms.Timer(this.components);
             this.SuspendLayout();
             // 
             // debugLabel1
             // 
             this.debugLabel1.BackColor = System.Drawing.SystemColors.ControlLightLight;
             this.debugLabel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.debugLabel1.Location = new System.Drawing.Point(226, 75);
+            this.debugLabel1.Location = new System.Drawing.Point(281, 136);
             this.debugLabel1.Name = "debugLabel1";
             this.debugLabel1.Size = new System.Drawing.Size(127, 54);
             this.debugLabel1.TabIndex = 0;
             this.debugLabel1.Text = "debugLabel1";
             this.debugLabel1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.debugLabel1.Click += new System.EventHandler(this.debugLabel1_Click);
             // 
             // playerBabyImageLabel
             // 
             this.playerBabyImageLabel.BackColor = System.Drawing.SystemColors.ControlLightLight;
             this.playerBabyImageLabel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.playerBabyImageLabel.Location = new System.Drawing.Point(171, 236);
+            this.playerBabyImageLabel.Location = new System.Drawing.Point(25, 49);
             this.playerBabyImageLabel.Name = "playerBabyImageLabel";
-            this.playerBabyImageLabel.Size = new System.Drawing.Size(127, 54);
+            this.playerBabyImageLabel.Size = new System.Drawing.Size(167, 107);
             this.playerBabyImageLabel.TabIndex = 1;
             this.playerBabyImageLabel.Text = "label1";
             this.playerBabyImageLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 50;
+            this.timer1.Tick += new System.EventHandler(this.tick);
+            // 
+            // timerTitlescreenEnter
+            // 
+            this.timerTitlescreenEnter.Interval = 2000;
+            this.timerTitlescreenEnter.Tick += new System.EventHandler(this.timerTitlescreenEnter_Tick);
             // 
             // Form1
             // 
@@ -62,7 +77,8 @@
             this.Controls.Add(this.playerBabyImageLabel);
             this.Controls.Add(this.debugLabel1);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = " ";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyPress);
             this.ResumeLayout(false);
 
         }
@@ -71,6 +87,8 @@
 
         private System.Windows.Forms.Label debugLabel1;
         private System.Windows.Forms.Label playerBabyImageLabel;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Timer timerTitlescreenEnter;
     }
 }
 
